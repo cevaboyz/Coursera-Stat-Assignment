f35ca8e91950c3c9adc266a3b235db35ab53ba19
cevaboyz <andrea.castagna1994@gmail.com>
2021-08-02 17:44:43 +0200

    Upload

---
+++ Guinea Pig Tooth Experiment 🦷.R
@@ -0,0 +1,41 @@
+
+## The Effect of Vitamin C on Tooth Growth in Guinea Pigs
+
+# Basic Inferential Data Analysis
+
+# Main question: DO DELIVERY METHOD AND/OR DOSAGE AFFECT TOOTH GROWTH IN GUINEA PIGS?
+
+# Overview 
+
+# The Tooth Growth data set contains information about an experiment regarding odontoblasts cells in 
+# 60 guinea pigs
+# 
+#_The response is the length of odontoblasts (cells responsible for tooth growth) in 60 guinea pigs._
+#_Each animal received one of three dose levels of vitamin C (0.5, 1, and 2 mg/day) 
+#_by one of two delivery methods, orange juice or ascorbic acid (a form of vitamin C and coded as VC)._
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+data("ToothGrowth")
+
+head(ToothGrowth)
